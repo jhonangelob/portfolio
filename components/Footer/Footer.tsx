787510299ff9283b38654e9aa64292a4092686d9
jhonangelo <jhon@vananz.com>
2023-07-19 09:29:50 +0800

    adjust footer design

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = ({}: Props): React.ReactElement => {
       className='flex-center relative w-full bg-background bottom-0 p-4'
       id='footer'
     >
-      <p className='text-white font-thin text-sm'>
+      <p className='text-gray font-thin text-xs md:text-sm'>
         © 2022 Jhon Angelo Bustarde
       </p>
     </footer>
